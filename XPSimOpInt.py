b2c42caa5837be2fdcc29e036478dbf0b121c2c0
daweed38 <davidalbert6938@gmail.com>
2025-02-01 00:51:29 +0100

    Initial Commit

---
+++ XPSimOpInt.py
@@ -0,0 +1,19 @@
+##################################################
+# Sim Open Interface X-Plane Plugin
+##################################################
+# REV 1.0
+# FarmerSoft © 2025
+# By Daweed
+##################################################
+
+# System Modules Import
+import sys
+import time
+import logging
+# import pickle
+# import socket
+# import selectors
+# import types
+# import signal
+
+
